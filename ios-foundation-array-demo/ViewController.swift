0e98929bd5c8363510d6e431d0ae92bff12c8d5f
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-04 17:11:30 +0900

    Add project

--- ios-foundation-array-demo/ViewController.swift
+++ ios-foundation-array-demo/ViewController.swift
@@ -9,29 +9,96 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //空の配列　配列要素の指定忘れずに！
+    var evenNumbers = Array<Int>()
+    var colars: [String] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-//空の配列を作る
-    private func createEmptyArray(){
-        //必ず要素の型を指定
-        let emptyArray1 = Array<Int>()
-        print(emptyArray1.isEmpty)
-        //true
-        let emptyArray2: [String] = []
-        print(emptyArray2.isEmpty)
-        //true
-
+        setUpArray()
     }
     
+    //配列の中身が空か調べる
+    private func isEmptyArray(){
+        
+        print(evenNumbers.isEmpty)
+        print(colars.isEmpty)
+    }
+
+    //配列に要素を入れる
+    private func setUpArray(){
+        
+        evenNumbers = [2, 4, 6, 8, 10, 12, 14, 16]
+        colars = ["red", "blue", "black", "white"]
+        print(colars.isEmpty)
+    }
+    
+    //配列の要素数を確認する
+    private func countElement(){
+    
+        let numberOfElement = colars.count
+        print(numberOfElement)
+    }
+    
+    //配列の要素を取り出す
     private func accessArrayElement(){
-    
-        let oddNumbers = [1, 3, 5, 7, 9, 11, 13, 15]
-    
-        for array in oddNumbers {
+        
+        for array in evenNumbers {
+            print(array)
+        }
+        //上と同じ結果
+        evenNumbers.forEach{array in
             print(array)
         }
     }
+    
+    //配列の最初と最後の要素取り出し
+    private func getFirstAndLastElements(){
+    
+        let firstElement = colars.first
+        print(firstElement)
+        let lastElement  = colars.last
+        print(lastElement)
+        
+        //配列が空の場合はnilを返す
+        var emptyArray:[String] = []
+        let testFirstElement = emptyArray.first
+        print(testFirstElement)
+    }
+    
+    //要素の追加
+    private func appendElement(){
+        //要素を最後に一つ追加
+        colars.append("yellow")
+        print(colars)
+        //複数の要素を最後に追加
+        colars.append(contentsOf:["green","orange"])
+        print(colars)
+        //特定の場所に追加
+        colars.insert("pink",at: 2)
+        print(colars)
+    }
+    
+    //配列の要素削除
+    private func removeElement(){
+        //指定したインデックスの要素削除
+        colars.remove(at: 0)
+        print(colars)
+        //最後の要素削除
+        colars.removeLast()
+        print(colars)
+    }
+    
+    //指定した要素のインデックスを取得し、別の要素と置き換える
+    private func replaceElement(){
+        
+        if let i = colars.index(of: "black"){
+            print(i)
+            colars[i] = "gray"
+            print(colars)
+        }
+    }
+    
 }
--- ios-foundation-array.playground/Contents.swift
+++ ios-foundation-array.playground/Contents.swift
@@ -2,4 +2,36 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+let oddNumbers = [1, 3, 5, 7, 9, 11, 13, 15]
+var colars = ["red", "blue", "black", "white"]
+let emptyArray1 = Array<Int>()
+
+for array in oddNumbers {
+    print(array)}
+
+oddNumbers.forEach{array in
+    print(array)
+}
+
+let firstElement = colars.first
+let lastElement  = colars.last
+let emptyArray   = emptyArray1.first
+
+colars.append("yellow")
+colars.append(contentsOf:["green","orange"])
+colars.insert("pink",at: 2)
+
+colars.remove(at: 0)
+print(colars)
+colars.removeLast()
+print(colars)
+
+//指定した要素が何番目か
+if let i = colars.index(of: "black"){
+    colars[i] = "gray"
+}
+
+print(colars)
+
+colars.count
